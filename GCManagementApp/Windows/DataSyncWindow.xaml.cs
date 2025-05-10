1c714c8e7aa2db67c0ad88c89213b7a30277a070
xArshy <emamdeearshad01@gmail.com>
2025-05-10 22:44:58 +0400

    Add project files.

--- GCManagementApp/Windows/DataSyncWindow.xaml.cs
+++ GCManagementApp/Windows/DataSyncWindow.xaml.cs
@@ -20,100 +20,100 @@ using System.Windows.Shapes;
 
 namespace GCManagementApp.Windows
 {
-    /// <summary>
-    /// Interaction logic for MessageDialog.xaml
-    /// </summary>
+	/// <summary>
+	/// Interaction logic for MessageDialog.xaml
+	/// </summary>
     public partial class DataSyncWindow : Window, INotifyPropertyChanged
     {
-        public ICommand OpenHelpCommand { get; set; }
+		public ICommand OpenHelpCommand { get; set; }
 
-        private Window _mainWindow;
+		private Window _mainWindow;
         public Window MainWindow
         {
-            get => _mainWindow;
-            set => SetProperty(ref _mainWindow, value);
-        }
+			get => _mainWindow;
+			set => SetProperty(ref _mainWindow, value);
+			}
 
-        private Action _closeAction;
+		private Action _closeAction;
         public Action CloseAction
         {
-            get => _closeAction;
-            set => SetProperty(ref _closeAction, value);
-        }
+			get => _closeAction;
+			set => SetProperty(ref _closeAction, value);
+			}
 
         public DataSyncWindow(Window mainWindow)
         {
-            InitializeComponent();
-            DataContext = this;
+			InitializeComponent();
+			DataContext = this;
 
-            OpenHelpCommand = new RelayCommand(OpenHelp);
+			OpenHelpCommand = new RelayCommand(OpenHelp);
 
-            Closing += OnWindowClosing;
-            Loaded += OnWindowLoaded;
-            MainWindow = mainWindow;
-            this.Topmost = true;
-        }
+			Closing += OnWindowClosing;
+			Loaded += OnWindowLoaded;
+			MainWindow = mainWindow;
+			this.Topmost = true;
+			}
 
         private void OnWindowLoaded(object sender, EventArgs e)
         {
-            Loaded -= OnWindowLoaded;
+			Loaded -= OnWindowLoaded;
             if (MainWindow != null)
             {
-                MainWindow.WindowState = WindowState.Minimized;
-            }
-        }
+				MainWindow.WindowState = WindowState.Minimized;
+				}
+			}
 
         private void OnWindowClosing(object sender, CancelEventArgs e)
         {
-            Closing -= OnWindowClosing;
+			Closing -= OnWindowClosing;
             if (CloseAction != null)
             {
-                CloseAction();
-            }
+				CloseAction();
+				}
             if (MainWindow != null)
             {
-                MainWindow.WindowState = WindowState.Maximized;
-            }
+				MainWindow.WindowState = WindowState.Maximized;
+				}
 
-            var wndsCount = EmulatorConnectionInfo.RegionWindowList.Count;
+			var wndsCount = EmulatorConnectionInfo.RegionWindowList.Count;
             for (int w = wndsCount - 1; w >= 0; w--)
             {
                 try
                 {
-                    EmulatorConnectionInfo.RegionWindowList[w].Close();
-                }
-                catch { }
-            }
-        }
+					EmulatorConnectionInfo.RegionWindowList[w].Close();
+					}
+				catch { }
+				}
+			}
 
         private void OpenHelp(object param)
         {
-            Process.Start(new ProcessStartInfo { FileName = "https://docs.google.com/document/d/1RAKXqxO965z5ZP39vVxMPz7ytqPJHVWcp6CTIXjCt1c", UseShellExecute = true });
-        }
+			Process.Start(new ProcessStartInfo { FileName = "https://docs.google.com/document/d/1RAKXqxO965z5ZP39vVxMPz7ytqPJHVWcp6CTIXjCt1c", UseShellExecute = true });
+			}
 
-        #region PC
+		#region PC
 
-        public event PropertyChangedEventHandler PropertyChanged = null!;
+		public event PropertyChangedEventHandler PropertyChanged = null!;
 
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null!)
         {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+			}
 
         protected virtual void OnPropertyChanged<T>(Expression<Func<T>> raiser)
         {
             var propName = ((MemberExpression)raiser?.Body!)?.Member.Name;
-            OnPropertyChanged(propName!);
-        }
+			OnPropertyChanged(propName!);
+			}
 
         protected bool SetProperty<T>(ref T field, T value, [CallerMemberName] string name = null!)
         {
             if (EqualityComparer<T>.Default.Equals(field, value)) return false;
-            field = value;
-            OnPropertyChanged(name);
-            return true;
-        }
+			field = value;
+			OnPropertyChanged(name);
+			return true;
+			}
 
-        #endregion
-    }
-}
+		#endregion
+		}
+	}
